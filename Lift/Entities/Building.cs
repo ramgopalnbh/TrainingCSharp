a9107e8ee2581a439ee20cbdb297e193d58bd833
RamGopal <56515607+ramgopalnbh@users.noreply.github.com>
2021-04-16 21:00:15 +0530

    Lift Problem

---
+++ Lift/Entities/Building.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Lift.Enums;
+
+namespace Lift.Entities
+{
+    public class Building
+    {
+        public Floor[] Floors { get; set; }
+
+        public Lift Lift { get; set; }
+
+        public Building(int liftCapacity, int[][] floorAndPeopleComposition)
+        {
+            Floors = floorAndPeopleComposition.Select((floorComposition, floorNumber) =>
+            {
+                var floor = new Floor(floorNumber, floorComposition);
+                floor.ButtonPressedForCallingTheLift += this.LiftRequested;
+                return floor;
+            }).ToArray();
+
+            Lift = new Lift(liftCapacity);
+
+            Lift.LiftArriverAtAFloor += LiftArrivedAtAFloor;
+        }
+        public Building(){}
+
+        public void LiftRequested(Direction direction, int floorNumberRequestedOn)
+        {
+        }
+
+        public void LiftArrivedAtAFloor(int floorNumber)
+        {
+            var floor = this.Floors.Single(floor => floor.FloorNumber == floorNumber);
+            floor.LiftHasArrived(this.Lift);
+           
+        }
+    }
+}
---
+++ Lift/Entities/Lift.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lift.Enums;
+
+namespace Lift.Entities
+{
+    public class Lift
+    {
+        #region "Events"
+
+        public event LiftArrivedAtAFloor LiftArriverAtAFloor;
+
+        #endregion
+
+        #region "Data"
+        public int Capacity { get; }
+        public List<Person> People { get; set; }
+        public int CurrentFloor { get; set; }
+        int topFloor = 10;
+        int GroundFloor = 0;
+        public Direction LiftDirection { get; set; }
+
+        #endregion
+
+        public Lift(int capacity)
+        {
+            this.CurrentFloor = 0;
+            this.Capacity = capacity;
+            this.LiftDirection = Direction.Stationary;
+        }
+
+        public void Start()
+        {
+            this.LiftDirection = Direction.GoingUp;
+            this.LiftArriverAtAFloor(this.CurrentFloor);
+          
+        }
+        Building LiftOperations = new Building();
+        public void OnboardPeople(List<Person> people)
+        {
+            this.People.AddRange(people);
+        }
+
+        public List<Person> OffboardPeople(int floorNumber)
+        {
+            var peopleToOffboard = this.People.Where(p => p.DestinationFloor == floorNumber).ToList();
+            this.People = this.People.Where(p => p.DestinationFloor != floorNumber).ToList();
+            return peopleToOffboard;
+        }
+
+        public int GetAvailableCapacity()
+        {
+            return this.Capacity - this.People.Count;
+        }
+        //Assuming that people will enter in sequence means if there are two people and they want go on 6 and 7 respectively so first
+        //person on 6 will enter then 7th one will enter
+        private void MoveUp()
+        {
+            if(this.LiftDirection==Direction.GoingUp && this.CurrentFloor!=topFloor)
+            {
+                var NextFloor = CurrentFloor;
+                this.People.ForEach(person =>
+                {
+                    if (person.DirectionToGoIn==this.LiftDirection)
+                    {
+                        NextFloor = person.DestinationFloor;
+                        LiftOperations.LiftArrivedAtAFloor(CurrentFloor);
+
+                        this.CurrentFloor = NextFloor;
+                    }
+                });
+
+
+            }
+            else if(this.CurrentFloor==topFloor)
+            {
+                if (this.People.Count > 1)
+                    MoveDown();
+                else
+                    CurrentFloor = GroundFloor;
+            }
+
+        }
+
+        private void MoveDown()
+        {
+            if (this.LiftDirection == Direction.GoingDown && this.CurrentFloor != GroundFloor)
+            {
+                var NextFloor = CurrentFloor;
+                this.People.ForEach(person =>
+                {
+                    if (person.DirectionToGoIn == this.LiftDirection)
+                    {
+                        NextFloor = person.DestinationFloor;
+                        LiftOperations.LiftArrivedAtAFloor(CurrentFloor);
+                        this.CurrentFloor = NextFloor;
+                    }
+                });
+
+                this.CurrentFloor = NextFloor;
+
+            }
+            else if (this.CurrentFloor == GroundFloor)
+            {
+                if(this.People.Count>1)
+                MoveUp();
+            }
+
+        }
+    }
+}
