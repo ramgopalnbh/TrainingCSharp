3cebc65a7305ad112b96fbeed10fcaf5d453b814
RamGopal <56515607+ramgopalnbh@users.noreply.github.com>
2021-04-19 09:48:05 +0530

    Product_Catalog

---
+++ Product-Catalog/Assignment2/Assignment2/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2.Entities
+{
+    public class Product
+    {
+        public static int ID = 1;
+        public int Product_ID { get; set; }
+        public static int GenerateProductId()
+        {
+            return ID++;
+        }
+        public string product_Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string ProductShortCode { get; set; }
+        public List<Category> ProductCategory { get; set; }
+        public Product()
+        {
+            this.ProductCategory = new List<Category>();
+        }
+        public string ProductDescription { get; set; }
+        public int Selling_Price { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return this.Product_ID + this.product_Name + this.Manufacturer + this.
+        //}
+
+
+    }
+}
---
+++ Product-Catalog/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Assignment2.Entities;
+using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please Select Option");
+            Console.WriteLine("a. Category");
+            Console.WriteLine("b. Product");
+            Console.WriteLine("c. Exit App!");
+
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            switch(ch)
+            {
+                case 'a':
+                    CategoryOperation.CategoryOperationMenu();
+                    break;
+                case 'b':
+                    ProductOperation.ProductOperationMenu();
+                    break;
+                case 'c':
+                    return;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+        }
+    }
+}
